c84d7ef3e0b40687bc7f132a8a606206a3c06dba
transhapHigsn <fet.prashantsingh@gmail.com>
2020-10-11 02:44:07 +0530

    feat: added algo with empirical solution

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## SQL Exercise
 
-- Inside `graph` folder.
+- Solution is inside `graph` folder.
 - Create Postgres Container ```docker run -d -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=postgres --network host postgres:alpine```
 - Connect to db: ```pgcli postgres://postgres:postgres@localhost:5432/postgres```
 - Create Schema and entries ```\i ~/path/animated-googles/graph/create.sql```
@@ -10,3 +10,9 @@
 - Drop everything ```\i ~/path/animated-googles/graph/drop.sql```
 
 ## Algo Exercise
+
+- Solution is inside `algo` folder.
+- To directly run solution, ```python3 main.py```
+- Create virtual environment ```python3 -m venv .venv```
+- Install dependencies ```pip install -r algo/requirements.txt```
+- To check plot, and run code in jupyterlab, ```jupyterlab```
---
+++ algo/main.py
@@ -0,0 +1,96 @@
+import string
+import random
+
+# Count of alternate vowel consonant sequence
+
+# sample
+
+# DOCOMO - 15
+# PRASHANT - 6
+# GOD - 3
+
+vowels = {
+    'a': True,
+    'e': True,
+    'i': True,
+    'o': True,
+    'u': True
+}
+# 6 - 1
+# 5 - 2
+# 4 - 3
+# 3 - 4
+# 2 - 5
+## 1 - 6
+
+# this is part of divide and conquer approach.
+# this part end up being extra code.
+def split_sequence(sequence, low, high):
+    if not sequence:
+        return ''
+
+    if high == low + 1:
+        return 'V' if vowels.get(sequence[low]) else 'C'
+
+    middle = (high + low)//2
+
+    return split_sequence(sequence[low:middle], 0, middle-low) + split_sequence(sequence[middle:high], 0, high-middle)
+
+# this is the real deal.
+# all you have to do is find length of substring matching constraint.
+# this will give you number of patterns that can be made from that.
+# for instance a substring of 6 characters matching constraints
+# will generate 15 possible sub patterns matching same constraint.
+# which is equal to sum(5+4+3+2+1) which further translates to formulae n(n+1)/2.
+def calculate_sum(n):
+    return (n-1)*(n)/2
+
+# here we are maintaining counter, which will increase until pattern is satisfying constraint
+# whenever constraint fails, calculate_sum is called to find maximum number of sub patterns within that length.
+# also, counter is reset to 1. finally sum of count of all sub patterns is returned.
+def create_final(s):
+    count, pattern_length, is_last_char_vowel = 0, 1, False
+    for i in s:
+        is_vowel = vowels.get(i, False)
+
+        if is_vowel == is_last_char_vowel:
+            if pattern_length > 1:
+                count += calculate_sum(pattern_length)
+                pattern_length = 1
+        else:
+            pattern_length += 1
+
+        is_last_char_vowel = is_vowel
+
+    if pattern_length > 1:
+        count += calculate_sum(pattern_length)
+
+    return int(count)
+
+
+def get_count(string):
+    # split_string = split_sequence(string, 0, len(string))
+    return create_final(string) # O(n)
+
+
+def generate_random_string(n):
+    return ''.join(random.choices(string.ascii_lowercase, k = n))
+
+
+if __name__ == '__main__':
+    c = get_count('god')
+    print(c)
+
+    c = get_count('prashant')
+    print(c)
+
+    c = get_count('docomo')
+    print(c)
+
+    # using random.choices() 
+    # generating random strings  
+    res = generate_random_string(10000)
+    c = get_count(res)
+    print(c)
+
+    print(res)
